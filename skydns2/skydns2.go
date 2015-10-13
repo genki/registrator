18aa33292ade9549c818aaa534cc3f4cf6a69cb1
TakiuchiGenki <genki@s21g.com>
2015-10-13 12:48:26 +0900

    Added skydns2s backend to communicate with etcd via ssl

--- skydns2/skydns2.go
+++ skydns2/skydns2.go
@@ -5,26 +5,43 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"os"
 
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/gliderlabs/registrator/bridge"
 )
 
 func init() {
-	bridge.Register(new(Factory), "skydns2")
+  bridge.Register(&Factory{Scheme: "http"}, "skydns2")
+  bridge.Register(&Factory{Scheme: "https"}, "skydns2s")
 }
 
-type Factory struct{}
+type Factory struct{
+  Scheme string
+}
 
 func (f *Factory) New(uri *url.URL) bridge.RegistryAdapter {
 	urls := make([]string, 0)
 	if uri.Host != "" {
-		urls = append(urls, "http://"+uri.Host)
+		urls = append(urls, f.Scheme+"://"+uri.Host)
+	} else {
+		urls = append(urls, f.Scheme+"://127.0.0.1:4001")
 	}
 
 	if len(uri.Path) < 2 {
 		log.Fatal("skydns2: dns domain required e.g.: skydns2://<host>/<domain>")
 	}
+
+  if f.Scheme == "https" {
+    cert := os.Getenv("ETCD_CERTFILE")
+    key := os.Getenv("ETCD_KEYFILE")
+    caCert := os.Getenv("ETCD_CAFILE")
+    client, err := etcd.NewTLSClient(urls, cert, key, caCert)
+    if err != nil {
+      log.Fatal("etcd: error creating tls client", err)
+    }
+    return &Skydns2Adapter{client:client, path:domainPath(uri.Path[1:])}
+  }
 
 	return &Skydns2Adapter{client: etcd.NewClient(urls), path: domainPath(uri.Path[1:])}
 }
